68e91c24d1166b62177e34c2ec8c67cc1c6980dd
CHraven <64539817+telomerica@users.noreply.github.com>
2022-12-29 16:28:10 +0100

    Update run.py

--- run.py
+++ run.py
@@ -11,7 +11,7 @@ for i in range(100):
     "bias":random.choice([0,-0.1,-0.2,-0.25]),"float":True}},
     {"method":perlin_,"args":{"octaves":random.choice([10,20,30]),"seed":random.randint(0,100),"object":1,
     "bias":random.choice([0,-0.2,-0.4,-0.5]),"float":True}},
-    {"method":random_,"args":{"probability":random.choice(0.5,0.1,0.15,0.2),"float":True}},
+    {"method":random_,"args":{"probability":random.choice([0.5,0.1,0.15,0.2]),"float":True}},
 
     ]
     ))
