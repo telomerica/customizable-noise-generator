7fa9ea4c09e5b7e7295234b8363d1a908a5ff585
CHraven <64539817+telomerica@users.noreply.github.com>
2023-01-08 18:31:51 +0100

    Add files via upload

--- load_d.py
+++ load_d.py
@@ -3,12 +3,10 @@ import os
 from array_builder import *
 from img_map import *
 
-ls = (os.listdir("saved_arrays"))
+ls = (os.listdir("saved_arrays"))[1:]
 chosen = random.choice(ls)
 mappe = np.load(f'{os.getcwd()}/saved_arrays/{chosen}',allow_pickle=True)
 
 print(mappe)
 print(chosen)
-
-
-draw_map(mappe).show()
+draw_map(mappe,color_dict=False,float=True).show()
